6e6e00f8d878925e776c040ee40ce1a5efeda6d9
MngSebastian <m.sebi97@yahoo.com>
2020-01-21 20:19:53 +0100

    Added Collision,Created a score system that also displays on screen and removed the white background from the PLayer1 and Player2 images.

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+/*Todays Goals:
+Fix the movement.
+Create collision detection.
+Research on how to make the map look cooler.
+Get 2 perks per player and make the perks spawn on the background.
+Add the lake and make the players disaper if they touch the lake. Add sounds.
+*/
+let s = "Score";
+let scoreCat = 0;
+let scoreDog = 0;
 let x = 1400;
 let y = 750;
 let bg;
+let lake;
 class Game {
   constructor(x, y) {
     this.players = "Markus";
@@ -15,23 +26,39 @@ const game = new Game();
 
 function preload() {
   player1.image = loadImage("/assets/player/cat/cat-walk/cat-walk.gif");
-  player2.image = loadImage("/assets/player/dog/dog.gif");
+  player2.image = loadImage("/assets/player/dog/dog1.gif");
 }
 
 function setup() {
   bg = loadImage("assets/background/background-img.jpg");
+  lake = loadImage("assets/background/bg-lake.jpg");
   createCanvas(game.canvasSize.x, game.canvasSize.y);
 }
 
 function draw() {
   // background(220, 220, 220);
   background(bg);
+  //Image(lake);
   //smooth movement -still testing
-  // if (keyIsDown(38)) {
-  //   moveUp();
-  // }
-  image(player1.image, player1.x, player1.y, 55, 55);
-  image(player2.image, player2.x, player2.y, 55, 55);
+
+  image(player1.image, player1.x, player1.y, player1.width, player1.height);
+  image(player2.image, player2.x, player2.y, player2.width, player2.height);
+
+  if (player1.collides(player2)) {
+    // console.log("Touched");
+    scoreDog += 0.04;
+    console.log(scoreDog.toFixed(2));
+  } else if (!player2.collides(player1)) {
+    scoreCat += 0.01;
+    console.log(scoreCat.toFixed(2));
+  }
+  textSize(32);
+  fill(255, 255, 255);
+  text(`Score Dog: ${scoreDog.toFixed(2)}`, width - width / 1.2, 30);
+
+  textSize(32);
+  fill(255, 255, 255);
+  text(`Score Cat: ${scoreCat.toFixed(2)}`, width - width / 2.8, 30);
 }
 
 //movement
--- player.js
+++ player.js
@@ -1,8 +1,10 @@
 class Player {
-  constructor(x, y) {
+  constructor(x, y, width, height) {
     this.y = y;
     this.x = x;
     this.image = "";
+    this.width = width;
+    this.height = height;
   }
 
   moveUp() {
@@ -21,14 +23,29 @@ class Player {
     this.x += 50;
   }
 
-  draw() {
-    image(this.image, this.y, this.x, 100, 100);
+  // draw() {
+  //   image(this.image, this.y, this.x, 100, 100);
+  // }
+  collides(obj) {
+    //check if obj collides with self
+    //self completely to the left || self completely to the right
+    if (this.x + this.width < obj.x || obj.x + this.width < this.x) {
+      return false;
+    }
+    // self completely to the top || self completely to the bottom
+    if (this.y + this.height < obj.y || obj.y + this.height < this.y) {
+      return false;
+    }
+    return true;
   }
 }
 
 //let player1; // cat
 //let player2; //dog
 
-////Creating the Players
-const player1 = new Player(300, 200);
-const player2 = new Player(0, 0);
+// // ////Creating the Players
+//cat
+const player1 = new Player(300, 200, 75, 75);
+
+//dog
+const player2 = new Player(0, 0, 100, 100);
